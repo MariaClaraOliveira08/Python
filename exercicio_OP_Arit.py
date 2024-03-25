700171013e4f5816cbacd3715cf1ce691731e65a
MariaClaraOliveira08 <159197888+MariaClaraOliveira08@users.noreply.github.com>
2024-03-25 15:15:58 -0300

    Exercicios 1 e 2 - OP Arit e PAR

---
+++ exercicio_OP_Arit.py
@@ -0,0 +1,26 @@
+import time
+N1 = float(6)
+N2 = float(2)
+
+# Adição
+print("Adição: ", N1 + N2)
+
+#Subtração
+print("Subtração: ", N1 - N2)
+
+#Multiplicação
+print("Multiplicação: ", N1 * N2)
+
+#Divisão
+print("Divisão: ", N1 / N2)
+
+#Divisão inteira
+print("Divisão inteira: ", N1 // N2)
+
+#Resto da divisão
+print("Resto da divisão: ", N1 % N2)
+
+#Potência
+print("Potência: ", N1 ** N2)
+
+time.sleep(5)
---
+++ exercicio_PAR.py
@@ -0,0 +1,13 @@
+import time
+
+n = float(input("Digite um número inteiro positivo: "))
+if(n >= 0):
+    "Digite um número positivo"
+
+if n % 2 == 0:
+     print('"P-A-R!!!"')
+
+else:
+    print('"Tente outra vez!')
+
+time.sleep(3)
